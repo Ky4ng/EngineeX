c977b21ac40b9d7897aec554d22ec595c34a42ee
ZhenYangLiew <zhenyang0904@1utar.my>
2025-09-19 10:35:29 +0800

    add all file to new repo

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -29,32 +29,34 @@ func _on_level_4_pressed():
 func _on_level_5_pressed():
 	get_tree().change_scene_to_file("res://Level/Level_5.tscn")
 	print("Level 5 pressed")
+	
+	
 
 
-func _on_level_6_pressed():
-	get_tree().change_scene_to_file("res://Level/Level_6.tscn")
-	print("Level 6 pressed")
-
-
-func _on_level_7_pressed():
-	get_tree().change_scene_to_file("res://Level/Level_7.tscn")
-	print("Level 7 pressed")
-
-
-func _on_level_8_pressed():
-	get_tree().change_scene_to_file("res://Level/Level_8.tscn")
-	print("Level 8 pressed")
-
-
-func _on_level_9_pressed():
-	get_tree().change_scene_to_file("res://Level/Level_9.tscn")
-	print("Level 9 pressed")
-
-func _on_level_10_pressed():
-	get_tree().change_scene_to_file("res://Level/Level_10.tscn")
-	print("Level 10 pressed")
 
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://Level/Level_11.tscn")
 	print("Level 11 pressed")
+
+
+func _on_level_6_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/Level_6.tscn")
+	print("Level 6 pressed")
+	
+func _on_level_7_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/Level_7.tscn")
+	print("Level 7 pressed")
+
+func _on_level_8_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/Level_8.tscn")
+	print("Level 8 pressed")
+
+func _on_level_9_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/Level_9.tscn")
+	print("Level 9 pressed")
+
+
+func _on_level_10_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/Level_10.tscn")
+	print("Level 10 pressed")
